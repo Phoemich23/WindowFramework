e5b32eb9a0498e166ab26b9889701c4c2e400746
Luke <toad1359@users.noreply.github.com>
2015-05-25 10:35:24 -0500

    Update Example.cpp

--- Example.cpp
+++ Example.cpp
@@ -2,7 +2,9 @@
 int main() {
     newBlankWindow(500, 500);
     while (!closed) {
-
+        displayOBJ(openOBJ("/home/user/test.obj"));
+        sleep(500);
+        removeOBJ(0);
     }
     return(0);
 }
