29a3ed156ab03f2768dc8af03a26f4f1d014552a
Luke <toad1359@users.noreply.github.com>
2015-05-25 20:32:42 -0500

    Update Linux.h

--- Linux.h
+++ Linux.h
@@ -21,8 +21,8 @@ static bool mtlused = false;
 extern bool minimized = false;
 static std::vector<std::string> mtlIndexer;
 //needs function for this v & ^
+extern char lastkey; //need to turn this into
 //needs resizing support & mouse support
-extern char lastkey;
 extern bool mouseClicked = false;
 extern double mouseLocX;
 extern double mouseLocY;
@@ -30,9 +30,9 @@ static XEvent event;
 
 struct pointColor
 {
-    double x, y, z;
-    std::string hex;
- };
+    double x=-1, y=-1, z=-1;
+    std::string hex = "#008000";
+};
 
 std::vector<std::vector<pointColor>> mtlColors;
 
@@ -46,25 +46,21 @@ struct Line
     double x2, y2, z2, w2;
 };
 
-struct Shape
-{
-    std::vector<Line> lines;
-};
 struct OBJ
 {
     std::vector<OBJv> points;
     //points turns into lines
     std::vector<Line> lines;
-    //^^ needs to be sorted into shapes
-    std::vector<Shape> shapes;
-    std::vector<pointColor> colors; //Hex values in string format per shape
-    int xlength, ylength, zlength;
-    int x1, x2, y1, y2, z1, z2; //closest and farthest points on each axis
+    std::vector<pointColor> colors; //Hex values in string format per pixel, defaultly green
+    double xlength, ylength, zlength;
+    double x1, x2, y1, y2, z1, z2; //closest and farthest points on each axis
+    int vcount = 0, vncount = 0, vtcount = 0;
 };
 
 static std::vector<OBJ> scene;
 
-struct point3d {
+struct point3d
+{
     int x, y, z;
 };
 
@@ -72,18 +68,58 @@ static void checkEvents ()
 {
     while(true)
     {
+        /*
+        Quote Originally Posted by sandbucket  View Post
+        anybody know where i can find a list of possible values the XEvent's type variable?
+        Theyr'e listed in the /usr/include/X11/X.h header file:
+        Code :
+        #define KeyPress                2
+        #define KeyRelease              3
+        #define ButtonPress             4
+        #define ButtonRelease           5
+        #define MotionNotify            6
+        #define EnterNotify             7
+        #define LeaveNotify             8
+        #define FocusIn                 9
+        #define FocusOut                10
+        #define KeymapNotify            11
+        #define Expose                  12
+        #define GraphicsExpose          13
+        #define NoExpose                14
+        #define VisibilityNotify        15
+        #define CreateNotify            16
+        #define DestroyNotify           17
+        #define UnmapNotify             18
+        #define MapNotify               19
+        #define MapRequest              20
+        #define ReparentNotify          21
+        #define ConfigureNotify         22
+        #define ConfigureRequest        23
+        #define GravityNotify           24
+        #define ResizeRequest           25
+        #define CirculateNotify         26
+        #define CirculateRequest        27
+        #define PropertyNotify          28
+        #define SelectionClear          29
+        #define SelectionRequest        30
+        #define SelectionNotify         31
+        #define ColormapNotify          32
+        #define ClientMessage           33
+        #define MappingNotify           34
+        #define GenericEvent            35
+        */
         XNextEvent(dis ,&event);
         switch (event.type)
         {
         case ClientMessage:
             closed = true;
             break;
-            //cases for other events
-        }
+        case //cases for other events
+            }
     }
     return;
 }
-void newBlankWindow(int x, int y)
+void newBlankWindow(int x, int y, int z)
 {
     dis = XOpenDisplay(NULL);
     win = XCreateSimpleWindow(dis, RootWindow(dis, 0), 1, 1, x, y, \
@@ -93,6 +129,11 @@ void newBlankWindow(int x, int y)
     Atom wmDelete=XInternAtom(dis, "WM_DELETE_WINDOW", True);
     XSetWMProtocols(dis, win, &wmDelete, 1);
     std::thread (checkEvents).detach();
+}
+
+void grabPointer()
+{
+
 }
 
 static void mtllib(std::string line)
@@ -127,6 +168,7 @@ OBJ openOBJ(std::string filepath)
             OBJv v;
             iss >> v.x >> v.y >> v.z;
             iss.ignore();
+            //need knowledge on what w does
             if (iss)
             {
                 iss >> v.w;
@@ -136,6 +178,7 @@ OBJ openOBJ(std::string filepath)
                 v.w = 1;
             }
             object.points.push_back(v);
+            object.vcount++;
         }
         if(filepath.substr(0, 6).compare("mtllib"))
         {
@@ -161,14 +204,72 @@ OBJ openOBJ(std::string filepath)
                 }
             }
         }
+        if(filepath.substr(0,2).compare("s "))
+        {
+            continue;
+        }
+        if(filepath.substr(0,2).compare("g "))
+        {
+            continue;
+        }
+        if(filepath.substr(0,3).compare("vn "))
+        {
+            //need knowledge on what vn is
+            object.vncount++;
+        }
+        if(filepath.substr(0,2).compare("s "))
+        {
+            continue;
+        }
+        if(filepath.substr(0,2).compare("f "))
+        {
+            filepath+=" ";
+            filepath = filepath.substr(2, filepath.length() - 2);
+            bool first = true;
+            for(int i = 0; !filepath.compare(""); i++)
+            {
+                if(first)
+                {
+                    int slashLoc = filepath.find("/");
+                    int line = std::stoi(filepath.substr(0, slashLoc));
+                    filepath = filepath.substr(filepath.find(" ") + 1, filepath.length() - filepath.find(" ") + 1);
+                    point3d line1 = object.points[line-1];
+                    object.lines[i].x1 = line1.x;
+                    object.lines[i].y1 = line1.y;
+                    object.lines.[i]z1 = line1.z;
+                    int slashLoc = filepath.find("/");
+                    int line = std::stoi(filepath.substr(0, slashLoc));
+                    filepath = filepath.substr(filepath.find(" ") + 1, filepath.length() - filepath.find(" ") + 1);
+                    object.lines[i].x2 = line1.x;
+                    object.lines[i].y2 = line1.y;
+                    object.lines[i].z2 = line1.z;
+                }
+                else
+                {
+                    object.lines[i].x1 = object.lines[i-1].x2;
+                    object.lines[i].z1 = object.lines[i-1].z2;
+                    object.lines[i].y1 = object.lines[i-1].y2;
+                    int slashLoc = filepath.find("/");
+                    int line = std::stoi(filepath.substr(0, slashLoc));
+                    filepath = filepath.substr(filepath.find(" ") + 1, filepath.length() - filepath.find(" ") + 1);
+                    object.lines.x2 = line1.x;
+                    object.lines.y2 = line1.y;
+                    object.lines.z2 = line1.z;
+                }
+                first = false;
+            }
+        }
     }
+    return object;
 }
-void displayOBJ(OBJ toDisplay, bool shaderEnable, int x1, int x2, int y1, int y2, int z1, int z2)
+void displayOBJ(OBJ toDisplay, bool shaderEnable, double x1, double x2, double y1, double y2, double z1, double z2)
 {
     //calculate 2D view and display pixels with correct colors. Darken colors for the farther they are IF shade is true.
+    //darkness formula with col beinnnng a int inn hex format ((col & 0x7E7E7E) >> 1) | (col & 0x808080)
+
 }
 void removeOBJ(int toRemove)
 {
-    //stop displaying OBJ at given index
+    scene.erase(toRemove);
 }
 #endif
